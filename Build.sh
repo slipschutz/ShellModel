f0e98e64f0b3cc2d1c174d56e8e5a442928f7706
Samuel Lipschutz <lipschut@trout.nscl.msu.edu>
2014-04-24 15:24:32 -0400

    Makes the right matrix for 2 by 2 case
    G=1
    -1  -1
    -1  1

--- Build.sh
+++ Build.sh
@@ -5,4 +5,4 @@
 CXX=`root-config --cxx`
 CFLAGS=`root-config --cflags`
 LDLIBS=`root-config --glibs`
-g++ main.C ${LDLIBS} ${CFLAGS} -std=c++11 -o main
+g++ main.C -g ${LDLIBS} ${CFLAGS} -std=c++11 -o main
--- main.C
+++ main.C
@@ -10,7 +10,7 @@
 using namespace std;
 
 int NumSingleParticleStates=4;
-
+double g=1;
 
 class SingleParticleState {
 public:
@@ -45,6 +45,7 @@ private:
 class SlaterDet {
 public:
   vector <bool> TheStates;
+  vector <int> TheOrderedStates;//things will be pushed on this 
   //  map <SingleParticleState,int> TheMapOfStates;
   int Phase;
   bool AddState(SingleParticleState s);
@@ -64,6 +65,7 @@ public:
   void Print();
 private:
   map <int,SingleParticleState> TheMapOfStates;
+  map <int, int> TheOrderedMap;
 };
 bool SlaterDet::AddState(SingleParticleState s){
   //Spot in Vector should be 2*(Level-1) + 1 for spin +
@@ -81,6 +83,8 @@ bool SlaterDet::AddState(SingleParticleState s){
   } else {
     TheStates[index]=1;
     TheMapOfStates[index]=s;
+    TheOrderedMap[index]=TheOrderedStates.size();
+    TheOrderedStates.push_back(index);
     return true;
   }
     
@@ -99,6 +103,8 @@ bool SlaterDet::RemoveState(SingleParticleState s){
   if ( TheStates[index]== 1){ //Already occupied
     TheStates[index]=0;
     TheMapOfStates.erase(index);
+    //TheOrderedStates.erase(TheOrderedStates.begin()+TheOrderedMap[index]);
+
     return true;
   } else { //Nothing there 
     //    Phase=0;
@@ -126,11 +132,13 @@ SlaterDet::SlaterDet(){
   Phase=1;
 }
 void SlaterDet::Print(){
+  cout<<"____________"<<endl;
   int size = TheStates.size();
   for (int i=0;i<size;i++){
     cout<<TheStates[i]<<" ";
   }
   cout<<endl;
+  cout<<"____________"<<endl;
 }
 
 
@@ -145,22 +153,35 @@ void BuildListOfSD(vector <SlaterDet> & List){
   temp2.AddState(SingleParticleState(2,1));
   temp2.AddState(SingleParticleState(2,-1));
   
-  List.push_back(temp2);
+    List.push_back(temp2);
 } 
 
-int GetPhase (vector <SingleParticleState> & theList){
-  int phase =1;
+int GetPhase (vector <int> list){
+  int phase=1;
+  bool loop=true;
+  while (loop){
+    loop=false;
 
+    for (int i=0;i<list.size()-1;i++){
+      if ( list[i+1]> list[i] ){//if there is an increase
+	int temp=list[i];
+	list[i]=list[i+1];
+	list[i+1]=temp;
+	phase=-1*phase;
+	loop=true;
+      }
+    }
+  }
 
-
+  return phase;
 }
 
 
 double GetMatrixElement(SlaterDet lhs,SlaterDet rhs){
   //Caluculate the 1 body term
-  cout<<"______________"<<endl;
+
   double totalOneBody=0;
-  for (int p=1;p<=NumSingleParticleStates;p++){
+  for (int p=1;p<=NumSingleParticleStates/2;p++){
     for (int s=0;s<=1;s++){
       int sigma=-9;
       if (s==0)
@@ -168,10 +189,8 @@ double GetMatrixElement(SlaterDet lhs,SlaterDet rhs){
       else if (s==1)
 	sigma=1;
 	
-
       SlaterDet rhsCopy=SlaterDet(rhs);
-      //Extract the SP states from the SD
-      //      vector <SingleParticleState> tempList=rhs.GetTheState();
+
       
       if(rhsCopy.RemoveState(SingleParticleState(p,sigma))==false){ //ie it failed to remove the state
 	//it is zero
@@ -180,21 +199,58 @@ double GetMatrixElement(SlaterDet lhs,SlaterDet rhs){
 	
       } else{ // it wasn't zero check if it over laps with lhs
 	if ( lhs == rhsCopy ){ 
-	  //a non-zero term
-	  totalOneBody=totalOneBody+(p-1);
-	  cout<<" p is "<<p<<endl;
-	  cout<<"sigma is "<<sigma<<endl;
-	  cout<<"***********"<<endl;
+	  //it overlaps
+	  // cout<<"The ORdered State ";
+	  // for (int i=0;i<rhsCopy.TheOrderedStates.size();i++){
+	  //   cout<<"I = "<<i<<" "<<rhsCopy.TheOrderedStates[i]<<" ";//rhsCopy.TheOrderedStates.size()-1-i];
+	  // }cout<<endl;
+	  // cout<<"TERM"<<endl;
+	  // cout<<"P IS "<<p<<endl;
+	  // cout<<"Sigma "<<sigma<<endl;
+	  // rhsCopy.Print();
+	  int phase = GetPhase(rhsCopy.TheOrderedStates);
+	  //	  cout<<"PHASE "<<phase<<endl;
+	  totalOneBody=totalOneBody+ (p-1);
+	  //	  int t;cin>>t;
 	}
 	
       }
-
-
     }//end s loop
-
   }//end p loop
 
+  
+  ///Calculate the two body term.
+  double TotalTwoBody=0;
+  for (int p=1;p<=NumSingleParticleStates/2;p++){
+    for (int q=1;q<=NumSingleParticleStates/2;q++){
+      SlaterDet rhsCopy=SlaterDet(rhs);
+      
 
+      //ad_p+ ad_p- a_q- a_q+
+  
+      if(rhsCopy.RemoveState(SingleParticleState(q,1))==false){ //ie it failed to remove the state
+	//it is zero
+      }else if(rhsCopy.RemoveState(SingleParticleState(q,-1))==false){ //ie it failed to add the state
+	//it is zero
+	
+      } else if (rhsCopy.AddState(SingleParticleState(p,-1))==false){
+	//it is zero
+      } else if (rhsCopy.AddState(SingleParticleState(p,1))==false){
+	//it is zero
+      } else{ // it wasn't zero check if it over laps with lhs
+	
+      	if ( lhs == rhsCopy ){ 
+	  TotalTwoBody=TotalTwoBody -g;
+	}
+	
+      }
+    }
+  }
+  
+
+  // cout<<"Total One body "<<totalOneBody<<endl;
+  // cout<<"Two body "<<TotalTwoBody<<endl;
+  return totalOneBody+TotalTwoBody;
 }
 
 int main(){
@@ -204,12 +260,19 @@ int main(){
 
 
   for ( auto i : ListOfSD){
-    i.Print();
+    //    i.Print();
   }
 
   
+  vector <int> temp;
 
-  GetMatrixElement(ListOfSD[0],ListOfSD[0]);
+  
+  for (auto i :ListOfSD){
+    for (auto j: ListOfSD){
+      cout <<GetMatrixElement(i,j)<<"  ";
+    }
+    cout<<endl;
+  }
   
 
 
