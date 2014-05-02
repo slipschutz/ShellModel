060d26eee3821a6bc9e2074e55557b5629a621c3
Samuel Lipschutz <lipschut@trout.nscl.msu.edu>
2014-05-02 11:44:01 -0400

    WORKING for project

--- main.C
+++ main.C
@@ -5,12 +5,16 @@
 #include <vector>
 #include <string>
 #include <map>
-//#include "TMatrixD.h"
-#include "Operator.h"
+#include "TMath.h"
+
+#include "TMatrixD.h"
+//#include "Operator.h"
 using namespace std;
 
-int NumSingleParticleStates=4;
+int NumSingleParticleStates=8;
+int NumParticles =4;
 double g=1;
+vector <int> Sort(vector <int> );
 
 class SingleParticleState {
 public:
@@ -47,6 +51,7 @@ public:
   vector <bool> TheStates;
   vector <int> TheOrderedStates;//things will be pushed on this 
   //  map <SingleParticleState,int> TheMapOfStates;
+  void Clear(){TheMapOfStates.clear();TheOrderedMap.clear();TheStates.clear();TheOrderedStates.clear();}
   int Phase;
   bool AddState(SingleParticleState s);
   bool RemoveState(SingleParticleState s);
@@ -142,19 +147,193 @@ void SlaterDet::Print(){
 }
 
 
+ 
 void BuildListOfSD(vector <SlaterDet> & List){
-  SlaterDet temp1;
-  temp1.AddState(SingleParticleState(1,1));
-  temp1.AddState(SingleParticleState(1,-1));
   
-  List.push_back(temp1);
+  int NumPLevels=NumSingleParticleStates/2;
+  int NumParticlePairs=2;
+  map<vector<int>,bool> tempMap;
+  
+  double NumSD = TMath::Factorial(NumPLevels)/( TMath::Factorial(NumParticlePairs)*TMath::Factorial(NumPLevels-NumParticlePairs));
+  cout<<"There should be "<<NumSD<<" Slater Dets"<<endl;
 
-  SlaterDet temp2;
-  temp2.AddState(SingleParticleState(2,1));
-  temp2.AddState(SingleParticleState(2,-1));
+ 
+  for (int p=1;p<=NumPLevels;p++){
+    SlaterDet temp;
+    temp.AddState(SingleParticleState(p,1));
+    temp.AddState(SingleParticleState(p,-1));
+    for (int i=0;i<NumParticlePairs-1;i++){
+      int count=0;
+      vector <int> t; 
+      for (int j=1;j<=NumPLevels;j++){
+
+
+	cout<<"IN J LOOP"<<endl;
+	SlaterDet copy=SlaterDet(temp);
+	bool b1=copy.AddState(SingleParticleState(j,1));
+	bool b2=copy.AddState(SingleParticleState(j,-1));
+	//check to see if we tried to add a state twice
+	if (b1 == true && b2 == true ){//only if both succeeded
+	  //j != p at this point
+	  temp.AddState(SingleParticleState(j,1));
+	  temp.AddState(SingleParticleState(j,-1));
+	
+	  t.push_back(p);
+	  t.push_back(j); //these were the P-levels added 
+	  t=Sort(t);
+	  if (tempMap.count(t)==0){
+	    //Victory
+	    cout<<"Victory"<<endl;
+	    List.push_back(temp);
+	    count++;
+	    temp.Print();	    
+	    tempMap[t]=true;
+	    temp.Clear();
+	  }//end count if
+	}//end b1 b2 if	
+	if (count==1){
+	  j=NumPLevels+3;
+	  count=0;
+	}
+      }//end j loop
+    }//end i loop
+  } //end p loop
   
-    List.push_back(temp2);
-} 
+
+}//end
+
+
+void BuildListOfSD2(vector <SlaterDet> & List){
+  
+  int NumPLevels=NumSingleParticleStates/2;
+  int NumParticlePairs=NumParticles/2;
+  map<vector<int>,bool> tempMap;
+  
+  double NumSD = TMath::Factorial(NumPLevels)/( TMath::Factorial(NumParticlePairs)*TMath::Factorial(NumPLevels-NumParticlePairs));
+  cout<<"There should be "<<NumSD<<" Slater Dets"<<endl;
+
+
+  if (NumParticlePairs== 1){
+    for (int p=1;p<=NumPLevels;p++){
+      SlaterDet temp;
+      temp.AddState(SingleParticleState(p,1));
+      temp.AddState(SingleParticleState(p,-1));
+      List.push_back(temp);
+    }
+  } else if (NumParticlePairs==2){
+    for (int p1=1;p1<=NumPLevels;p1++){
+      for (int p2=1;p2<=NumPLevels;p2++){
+	SlaterDet temp;
+	vector<int> t;
+	bool b1=temp.AddState(SingleParticleState(p1,1));
+	temp.AddState(SingleParticleState(p1,-1));
+
+	bool b2=temp.AddState(SingleParticleState(p2,1));
+	temp.AddState(SingleParticleState(p2,-1));
+	if (b1 && b2){
+	  t.push_back(p1);
+	  t.push_back(p2);
+	  t=Sort(t);
+
+	  if (tempMap.count(t)==0){
+	    List.push_back(temp);
+	    tempMap[t]=true;
+	  }
+	}
+      }
+    }
+
+  }else if (NumParticlePairs ==3 ){
+    for (int p1=1;p1<=NumPLevels;p1++){
+      for (int p2=1;p2<=NumPLevels;p2++){
+	for (int p3=1;p3<=NumPLevels;p3++){
+
+	  SlaterDet temp;
+	  vector<int> t;
+	  bool b1=temp.AddState(SingleParticleState(p1,1));
+	  temp.AddState(SingleParticleState(p1,-1));
+
+	  bool b2=temp.AddState(SingleParticleState(p2,1));
+	  temp.AddState(SingleParticleState(p2,-1));
+
+	  bool b3=temp.AddState(SingleParticleState(p3,1));
+	  temp.AddState(SingleParticleState(p3,-1));
+
+
+	  if (b1 && b2 && b3){
+	    t.push_back(p1);
+	    t.push_back(p2);
+	    t.push_back(p3);
+	    t=Sort(t);
+
+	    if (tempMap.count(t)==0){
+	      List.push_back(temp);
+	      tempMap[t]=true;
+	    }
+	  }
+	}
+      }
+    }
+  }else if (NumParticlePairs==4){
+    for (int p1=1;p1<=NumPLevels;p1++){
+      for (int p2=1;p2<=NumPLevels;p2++){
+	for (int p3=1;p3<=NumPLevels;p3++){
+	  for (int p4=1;p4<=NumPLevels;p4++){
+	    SlaterDet temp;
+	    vector<int> t;
+	    bool b1=temp.AddState(SingleParticleState(p1,1));
+	    temp.AddState(SingleParticleState(p1,-1));
+	    
+	    bool b2=temp.AddState(SingleParticleState(p2,1));
+	    temp.AddState(SingleParticleState(p2,-1));
+	    
+	    bool b3=temp.AddState(SingleParticleState(p3,1));
+	    temp.AddState(SingleParticleState(p3,-1));
+	    
+	    bool b4=temp.AddState(SingleParticleState(p4,1));
+	    temp.AddState(SingleParticleState(p4,-1));
+	    
+	    
+
+	    if (b1 && b2 && b3 &&b4){
+	      t.push_back(p1);
+	      t.push_back(p2);
+	      t.push_back(p3);
+	      t.push_back(p4);
+	      t=Sort(t);
+
+	      if (tempMap.count(t)==0){
+		List.push_back(temp);
+		tempMap[t]=true;
+	      }
+	    }
+	  }
+	}
+      }
+    }
+  } else if (NumParticlePairs==5){
+    throw "Not Done Yet";
+  } else if (NumParticlePairs==6){
+    throw "Not Done Yet";
+
+  }
+
+}
+
+
+vector <int> Sort(vector <int> input){
+  vector <int> temp(100,0);
+  vector <int> temp2;
+  for (auto i : input){
+    temp[i]=i;
+  }
+  for (auto j : temp ){
+    if (j!=0){
+      temp2.push_back(j);
+    }
+  }
+  return temp2;
+}
 
 int GetPhase (vector <int> list){
   int phase=1;
@@ -252,29 +431,70 @@ double GetMatrixElement(SlaterDet lhs,SlaterDet rhs){
   // cout<<"Two body "<<TotalTwoBody<<endl;
   return totalOneBody+TotalTwoBody;
 }
+#include "TVectorT.h"
+int main(int argc,char** argv){
 
-int main(){
+  if (argc ==2){
+    g=atof(argv[1]);
+      cout<<"G is "<<g<<endl;
+      cout<<"Num Particles? "<<endl;
+      cin>>NumParticles;
+      cout<<"Num SP?"<<endl;
+      cin>>NumSingleParticleStates;
+
+  } else if (argc == 4){
+    g=atof(argv[1]);
+    NumParticles=atoi(argv[2]);
+    NumSingleParticleStates=atoi(argv[3]);
+  } else {
+    cout<<"Usage: ./main g"<<endl;
+    cout<<"Usage: ./main g NumParticles NumSPStates"<<endl;
+    return -1;
+  }
+      
 
   vector <SlaterDet> ListOfSD;
-  BuildListOfSD(ListOfSD);
+  
+
+
+  BuildListOfSD2(ListOfSD);
 
 
   for ( auto i : ListOfSD){
-    //    i.Print();
+    i.Print();
   }
 
   
-  vector <int> temp;
 
+  TMatrixD theMatrix(ListOfSD.size(),ListOfSD.size());
   
-  for (auto i :ListOfSD){
-    for (auto j: ListOfSD){
-      cout <<GetMatrixElement(i,j)<<"  ";
+  for (int i=0;i<ListOfSD.size();i++){
+    for (int j=0;j<ListOfSD.size();j++){
+      theMatrix[i][j]=GetMatrixElement(ListOfSD[i],ListOfSD[j]);
+      
     }
-    cout<<endl;
   }
+  theMatrix.Print();
   
+  TVectorT <double> eigenValues;
+  TMatrixD result=theMatrix.EigenVectors(eigenValues);
 
+  result.Print();
+  
+  eigenValues.Print();
 
   return 0;
+
+  for (int i=0;i< theMatrix.GetNrows();i++){
+    cout<<"{";
+    for (int j=0;j<theMatrix.GetNcols();j++){
+      if (j<theMatrix.GetNcols()-1){
+	cout<<theMatrix[i][j]<<",";
+      } else {
+	cout<<theMatrix[i][j];
+      }
+    }
+    cout<<"},";
+  }
+
 }
